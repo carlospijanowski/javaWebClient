a6f46996816064fd28e7902c0994b85cf74489e0
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2023-11-30 14:03:11 -0300

    ajuste nas dependencias, deixando apenas a webflux e removendo a spring-starter

--- src/main/java/br/com/seteideias/consumer/ConsumerApplication.java
+++ src/main/java/br/com/seteideias/consumer/ConsumerApplication.java
@@ -2,11 +2,6 @@ package br.com.seteideias.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.function.ServerRequest;
 
 @SpringBootApplication
 public class ConsumerApplication {
